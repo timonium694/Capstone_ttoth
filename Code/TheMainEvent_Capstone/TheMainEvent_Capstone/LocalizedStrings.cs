6e170de2c8a9a3b1b2f2527937adf17a7977e03d
timonium694 <ttoth@student.neumont.edu>
2014-01-08 11:24:07 -0700

    Adding Source Project

---
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TheMainEvent_Capstone.Resources;
+
+namespace TheMainEvent_Capstone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
---
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/MapPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Device.Location;
+
+namespace TheMainEvent_Capstone
+{
+	public partial class MapPage : PhoneApplicationPage
+	{
+		public MapPage()
+		{
+			InitializeComponent();
+			Loaded += MapPage_Loaded;
+		}
+		void MapPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			UpdateMap();
+		}
+
+		private void UpdateMap()
+		{
+			MainMap.SetView(new GeoCoordinate(41.8988D, -87.6231D), 17D);
+		}
+
+	}
+}
