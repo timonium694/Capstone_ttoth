dd3c21ca5569f9fe90dd0f3a092c7a265cbb4bd5
timonium694 <ttoth@student.neumont.edu>
2014-01-29 11:23:11 -0700

    More Week 3

--- Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Model/ViewModels/ContactViewModel.cs
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Model/ViewModels/ContactViewModel.cs
@@ -8,6 +8,13 @@ namespace TheMainEvent_Capstone.Model.ViewModels
 {
 	class ContactViewModel
 	{
-
+		public string Id { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Phone { get; set; }
+		public string Bio { get; set; }
+		public string Name { get; set; }
+		public DateTime Birthday { get; set; }
 	}
 }
--- Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/CreateEvent.xaml.cs
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/CreateEvent.xaml.cs
@@ -8,15 +8,56 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using TheMainEvent_Capstone.Model.ViewModels;
 
 namespace TheMainEvent_Capstone.Pages
 {
 	public partial class CreateEvent : PhoneApplicationPage
 	{
+		ObservableCollection<ContactViewModel> Users = new ObservableCollection<ContactViewModel>();
 		public CreateEvent()
 		{
 			InitializeComponent();
+			this.AddUsers();
+			contacts.ItemsSource = Users;
+			city.Tap += this.TextBox_Tap;
+			address.Tap += this.TextBox_Tap;
+			state.Tap += this.TextBox_Tap;
+			cost.Tap += this.TextBox_Tap;
+			description.Tap += this.TextBox_Tap;
+			otherDetails.Tap += this.TextBox_Tap;
+			title.Tap += this.TextBox_Tap;
 		}
+		private void AddUsers()
+		{
+			Users.Add(new ContactViewModel()
+			{
+				Name = "John",
+				Phone = "330-766-0092",
+			});
+			Users.Add(new ContactViewModel()
+			{
+				Name = "John 1",
+				Phone = "330-766-0092",
+			});
+			Users.Add(new ContactViewModel()
+			{
+				Name = "John 2",
+				Phone = "330-766-0092",
+			});
+			Users.Add(new ContactViewModel()
+			{
+				Name = "John 3",
+				Phone = "330-766-0092",
+			});
+			Users.Add(new ContactViewModel()
+			{
+				Name = "John 4",
+				Phone = "330-766-0092",
+			});
+		}
+
 
 		private void OnTextInputStart(object sender, TextCompositionEventArgs e)
 		{
@@ -27,7 +68,7 @@ namespace TheMainEvent_Capstone.Pages
 		private void TextBox_Tap(object sender, System.Windows.Input.GestureEventArgs e)
 		{
 			TextBox box = (TextBox)sender;
-			if (box.Text.Equals("Title") || box.Text.Equals("City") || box.Text.Equals("State") || box.Text.Equals("Description") || box.Text.Equals("Street Address") || box.Text.Equals("Other Details"))
+			if (box.Text.Equals("Title") || box.Text.Equals("City") || box.Text.Equals("State") || box.Text.Equals("Description") || box.Text.Equals("Street Address") || box.Text.Equals("Other Details") || box.Text.Equals("0.00"))
 			{
 				box.Text = "";
 			}
