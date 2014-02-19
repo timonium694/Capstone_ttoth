b08dcd5030ff19486fbcb3f8e6e5209b07c69bf0
timonium694 <ttoth@student.neumont.edu>
2014-02-18 23:03:47 -0700

    Last push fixed added error handling for unhndled exceptions

--- Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EventPanorama.xaml.cs
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EventPanorama.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Windows.Input;
 
 namespace TheMainEvent_Capstone.Pages
 {
@@ -15,6 +16,31 @@ namespace TheMainEvent_Capstone.Pages
 		public EventPanorama()
 		{
 			InitializeComponent();
+		}
+		private void OnTextInputStart(object sender, TextCompositionEventArgs e)
+		{
+			scrollViewer.UpdateLayout();
+			scrollViewer.ScrollToVerticalOffset(tweetBox.ActualHeight);
+		}
+		private void attendButton_Click(object sender, RoutedEventArgs e)
+		{
+			//if (owner.MerchantEmail.Equals("none"))
+			//{
+			//	EventDAL ed = new EventDAL();
+			//	ed.AddAttendee(evm.ID, currentUser.User);
+			//}
+			//else if (!owner.MerchantEmail.Equals("none"))
+			//{
+			//	this.PayForEvent(owner.MerchantEmail);
+			//	EventDAL ed = new EventDAL();
+			//	ed.AddAttendee(evm.ID, currentUser.User);
+			//}
+
+		}
+
+		private void tweetBox_LostFocus(object sender, RoutedEventArgs e)
+		{
+
 		}
 	}
 }
