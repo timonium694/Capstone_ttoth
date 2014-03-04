997584f493fab096fa5d527f7bc278a0c8b1357c
timonium694 <ttoth@student.neumont.edu>
2014-03-03 21:54:29 -0700

    Update

--- Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EditEvent.xaml.cs
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EditEvent.xaml.cs
@@ -7,14 +7,149 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Collections.ObjectModel;
+using Parse;
+using TheMainEvent_Capstone.Model;
+using TheMainEvent_Capstone.DataAccessLayer;
+using TheMainEvent_Capstone.Model.ViewModels;
+using System.Windows.Input;
 
 namespace TheMainEvent_Capstone.Pages
 {
 	public partial class EditEvent : PhoneApplicationPage
 	{
+		ObservableCollection<ContactViewModel> Users = new ObservableCollection<ContactViewModel>();
+		ParseUser user;
 		public EditEvent()
 		{
 			InitializeComponent();
+			this.AddUsers();
+			this.SetupPage();
+		}
+
+		private void SetupPage()
+		{
+			if (ParseUser.CurrentUser != null)
+			{
+				user = ParseUser.CurrentUser;
+			}
+			descriptionBox.GotFocus += this.DescriptionBox_GotFocus;
+			otherDetailsBox.GotFocus += this.OtherDetailsBox_GotFocus;
+			descriptionBox.LostFocus += this.DescriptionBox_LostFocus;
+			otherDetailsBox.LostFocus += this.OtherDetailsBox_LostFocus;
+			titleBox.TextChanged += this.title_TextChanged;
+		}
+
+		
+		private async void AddUsers()
+		{
+			UserDAL ud = new UserDAL();
+			List<string> ids = new List<string>();
+			ids = await ud.GetContacts(ParseUser.CurrentUser.ObjectId);
+			foreach (string id in ids)
+			{
+				try
+				{
+					UserInfo ui = await ud.GetUserInfo(id);
+					Users.Add(new ContactViewModel()
+					{
+						Name = ui.FirstName + " " + ui.LastName,
+						User = ui.User,
+						Bio = ui.Bio,
+						Birthday = ui.Birthday,
+						Phone = ui.Phone,
+					});
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+				
+			}
+
+			contacts.ItemsSource = Users;
+		}
+
+
+		private void OnTextInputStart(object sender, TextCompositionEventArgs e)
+		{
+			scrollViewer.UpdateLayout();
+			scrollViewer.ScrollToVerticalOffset(descriptionBox.ActualHeight);
+		}
+
+		private void TextBox_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+		{
+			TextBox box = (TextBox)sender;
+			
+		}
+
+		private async void createButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Event ev = new Event()
+				{
+					City = cityBox.Text,
+					Address = addressBox.Text,
+					State = stateBox.Text,
+					Description = descriptionBox.Text,
+					OtherDetails = otherDetailsBox.Text,
+					Title = titleBox.Text,
+					Date = (DateTime)datePicker.Value,
+					Time = (DateTime)timePicker.Value
+				};
+				double cost = 0;
+				if (double.TryParse(costBox.Text, out cost))
+				{
+					ev.Cost = cost;
+				}
+				else
+				{
+					throw new Exception("Make sure the cost follows the format of a price e.g.: 9.50");
+				}
+
+				EventDAL ed = new EventDAL();
+				ed.CreateEvent(ev);
+				string creatorId = ParseUser.CurrentUser.ObjectId;
+				string eventToAdd = await ed.NewestEventFromUser(ParseUser.CurrentUser.ObjectId );
+				ed.SetOwner(eventToAdd, creatorId);
+				foreach (Object i in contacts.SelectedItems)
+				{
+					ContactViewModel con = (ContactViewModel)i;
+					ed.InviteUser(con.User, eventToAdd, creatorId);
+				}
+			}
+			catch (Exception ex)
+			{
+				NavigationService.Navigate(new Uri("/Pages/MapPage.xaml?msg=" + ex.Message, UriKind.Relative));
+			}
+			NavigationService.Navigate(new Uri("/Pages/MapPage.xaml", UriKind.Relative));
+		}
+
+		private void title_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			titleBlock.Text = titleBox.Text;
+		}
+
+
+		private void DescriptionBox_GotFocus(object sender, RoutedEventArgs e)
+		{
+			this.descGotFocusStoryboard.Begin();
+		}
+		private void DescriptionBox_LostFocus(object sender, RoutedEventArgs e)
+		{
+			this.descLostFocusStoryboard.Begin();
+		}
+
+
+
+		private void OtherDetailsBox_GotFocus(object sender, RoutedEventArgs e)
+		{
+			this.otherGotFocusStoryboard.Begin();
+		}
+		private void OtherDetailsBox_LostFocus(object sender, RoutedEventArgs e)
+		{
+			this.otherLostFocusStoryboard.Begin();
 		}
 	}
 }
--- Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EditUser.xaml.cs
+++ Code/TheMainEvent_Capstone/TheMainEvent_Capstone/Pages/EditUser.xaml.cs
@@ -7,6 +7,9 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using TheMainEvent_Capstone.Model;
+using TheMainEvent_Capstone.DataAccessLayer;
+using Parse;
 
 namespace TheMainEvent_Capstone.Pages
 {
@@ -15,6 +18,48 @@ namespace TheMainEvent_Capstone.Pages
 		public EditUser()
 		{
 			InitializeComponent();
+			phoneBox.Tap += this.TextBox_Tap;
+			lastNameBox.Tap += this.TextBox_Tap;
+			firstNameBox.Tap += this.TextBox_Tap;
+			bioBox.Tap += this.TextBox_Tap;
+
+			
+		}
+		private void TextBox_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+		{
+			TextBox box = (TextBox)sender;
+			if (box.Text.Equals("First Name") || box.Text.Equals("Last Name") || box.Text.Equals("Bio") || box.Text.Equals("Phone Number"))
+			{
+				box.Text = "";
+			}
+		}
+
+		private void doneButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				DateTime bday = (DateTime)datePicker.Value;
+				string firstName = this.firstNameBox.Text;
+				string lastName = this.lastNameBox.Text;
+				string phoneNumber = this.phoneBox.Text;
+				string bio = this.bioBox.Text;
+				UserInfo ui = new UserInfo()
+				{
+					FirstName = firstName,
+					LastName = lastName,
+					Phone = phoneNumber,
+					Bio = bio,
+					Birthday = bday,
+					User = ParseUser.CurrentUser.ObjectId,
+					MerchantEmail = "tim.toth13@gmail.com",
+					Email = ParseUser.CurrentUser.Email,
+				};
+				UserDAL ud = new UserDAL();
+				ud.CreateUserInfo(ui);
+			}
+			catch (Exception ex)
+			{
+			}
 		}
 	}
 }
